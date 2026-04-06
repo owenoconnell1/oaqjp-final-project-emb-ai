80f3e543fcb14290dedf296df9fac6dfe85eba6c
= <=>
2026-04-06 02:58:49 -0400

    emotion_detection.py updated

--- emotion_detection.py
+++ emotion_detection.py
@@ -3,7 +3,26 @@ import requests
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    json_input = { "raw_document": { "text": text_to_analyse } }
+    json_input = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json_input)
-    response_dictionary = json.loads(response)
+    response_dictionary = json.loads(response.text)
+    
+    emotions = response_dictionary["emotionPredictions"]['emotion']
+    anger_score = emotions["anger"]
+    disgust_score = emotions["disgust"]
+    fear_score = emotions["fear"]
+    joy_score = emotions["joy"]
+    sadness_score = emotions["sadness"]
+    score_list = {anger_score, disgust_score, fear_score,joy_score, sadness_score}
+    dominant_emotion = max(score_list)
+    return {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
+
+
     
