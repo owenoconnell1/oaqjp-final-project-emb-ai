a923adb3bce695f323ff66da2dcf5dfcfd5d040f
owen <owenoconnell58@gmail.com>
2026-04-07 02:22:39 -0400

    adding files

---
+++ server.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+app = Flask("Emotion Detector")
+@app.route("/emotionDetector")
+def detect_emotion():
+    text = request.args.get("text")
+    return jsonify(emotion_detector(text))
+if __name__ == "__main__":
+    app.run()
