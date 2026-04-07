6dee9c7db3f033ec1e30ec67e47808d0b303a22a
owen <owenoconnell58@gmail.com>
2026-04-07 03:09:43 -0400

    added files

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -5,6 +5,15 @@ def emotion_detector(text_to_analyze):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     json_input = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json=json_input, headers=headers)
+    if response.status_code !=400:
+        return {
+        'anger': 'None',
+        'disgust': 'None',
+        'fear': 'None',
+        'joy': 'None',
+        'sadness': 'None',
+        'dominant_emotion': 'None'
+    }
     response_dictionary = json.loads(response.text)
     emotions = response_dictionary["emotionPredictions"][0]["emotion"]
     anger_score = emotions["anger"]
@@ -12,8 +21,6 @@ def emotion_detector(text_to_analyze):
     fear_score = emotions["fear"]
     joy_score = emotions["joy"]
     sadness_score = emotions["sadness"]
-    if response.status_code !=200:
-        return None
     score_list = {
         'anger': anger_score,
         'disgust': disgust_score,
--- server.py
+++ server.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template, request, jsonify
+"""Flask server"""
+from flask import Flask, request, jsonify
 from EmotionDetection.emotion_detection import emotion_detector
 app = Flask("Emotion Detector")
 @app.route("/emotionDetector")
 def detect_emotion():
+    """Returns detected emotion"""
     text = request.args.get("text")
+    if text is None:
+        return "Invalid text! Please try again!"
     return jsonify(emotion_detector(text))
 if __name__ == "__main__":
     app.run()
