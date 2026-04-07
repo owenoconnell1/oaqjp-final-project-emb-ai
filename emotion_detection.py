777db7b944936a9a922485376b513767692b523d
owen <owenoconnell58@gmail.com>
2026-04-06 22:51:52 -0400

    emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -4,10 +4,9 @@ def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     json_input = { "raw_document": { "text": text_to_analyze } }
-    response = requests.post(url, json_input)
+    response = requests.post(url, json=json_input, headers=headers)
     response_dictionary = json.loads(response.text)
-    
-    emotions = response_dictionary["emotionPredictions"]['emotion']
+    emotions = response_dictionary["emotionPredictions"][0]["emotion"]
     anger_score = emotions["anger"]
     disgust_score = emotions["disgust"]
     fear_score = emotions["fear"]
